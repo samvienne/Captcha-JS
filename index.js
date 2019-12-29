00fc14ec86c7e0bd53e2b97482e481c66cf31bca
Samuel Vienne <samuelvienne@yahoo.fr>
2019-12-29 11:58:15 -0500

    index.js final

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* Here Keycodes are the one from the keyboard but you can do the same for the keypad*/
+
 let CaptchaValues = [
 	{ question: 'un plus trois', codes: [49,187,51], answer:'quatre'},
 	{ question: 'six divisé par deux', codes: [54,191,50], answer:'trois'},
@@ -23,13 +25,6 @@ function choseQuestion() {
 	const text = `Veuillez saisir sur le clavier <strong>${pickedQuestion.question}</strong> (échap pour recommencer votre saisie)`;
 	display(text, questionDiv);
 }
-
-function display(what, where) {
-	where.innerHTML = what;
-}
-
-window.addEventListener('keydown', handleKeyboardEvent);
-textResult.addEventListener('keydown', checkTextResult);
 
 
 function handleKeyboardEvent(evt) {
@@ -62,8 +57,30 @@ function computeResult() {
 	if (isHuman && !isGoodpressedKeys) {
 		display('Vous êtes humain mais avez fait une erreur dans la saisie', resultDiv);
 	} else if(isHuman && isGoodpressedKeys){
-		display('Merci votre saisie est correcte. Veuillez indiquer votre résultat ci-dessous et appuyer sur Entrée', resultDiv);
+		display('Merci votre saisie est correcte. Veuillez indiquer votre résultat ci-dessous EN LETTRES et appuyer sur Entrée', resultDiv);
 	}
+}
+
+function checkTextResult(evt) {
+	if(evt.keyCode >= 65 && evt.keyCode <= 90) {
+		resultInLetters.push(evt.key);
+	} else if(evt.keyCode === 8) {
+		resultInLetters.pop();
+	} else if(evt.keyCode === 13) {
+		if(resultInLetters.join('') === pickedQuestion.answer) {
+			display('Validation réussie :)',resultDiv);
+		} else {
+		display('Validation fausse', resultDiv);
+		}
+	} 
+}
+
+choseQuestion();
+
+/* Utility Fonctions */
+
+function display(what, where) {
+	where.innerHTML = what;
 }
 
 function compareArrays(a, b) {
@@ -75,19 +92,7 @@ function compareArrays(a, b) {
 	return true;
 }
 
-function checkTextResult(evt) {
-	if(evt.keyCode >= 65 && evt.keyCode <= 90) {
-		resultInLetters.push(evt.key);
-	} else if(evt.keyCode === 8) {
-		resultInLetters.pop();
-		console.log(resultInLetters);
-	} else if(evt.keyCode === 13) {
-		if(resultInLetters.join('') === pickedQuestion.answer) {
-			display('Validation réussie :)',resultDiv);
-		} else {
-		display('Validation fausse', resultDiv);
-		}
-	} 
-}
+/* Events */
 
-choseQuestion();
+window.addEventListener('keydown', handleKeyboardEvent);
+textResult.addEventListener('keydown', checkTextResult);
